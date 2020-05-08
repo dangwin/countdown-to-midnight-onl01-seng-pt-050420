0b9865ec944eaa2f7df0dfff2c4e440e50ab6c2c
Dang Nguyen <danghwin@gmail.com>
2020-05-08 03:24:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,6 +11,14 @@ puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    
+end
 
+puts "HAPPY NEW YEAR!"
+end
 
 
